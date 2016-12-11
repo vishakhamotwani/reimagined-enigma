11c76ea23d0191861faba46308239a57505ab944
Vishakha Motwani <vishakha.motwani@gmail.com>
2016-12-10 20:20:59 -0500

    Methods to create, add nodes and display the linked list

--- src/psychic/lamp/linkedlist/LinkedList.java
+++ src/psychic/lamp/linkedlist/LinkedList.java
@@ -11,9 +11,6 @@ public class LinkedList {
 
 	private Node head;
 	
-	/**
-	 * @return Node head
-	 */
 	public Node getHead() {
 		return head;
 	}
@@ -27,17 +24,61 @@ public class LinkedList {
 		this.head = head;
 	}
 
+	public LinkedList()
+	{
+		
+	}
+	
 	public LinkedList(Integer item)
     {
         head = new Node(item);
     }
 	
-	/**
-	 * @param args
-	 */
+	public void add(Integer data) 
+	{
+		//If it is the first element in the list
+		if(head == null)
+		{
+			head = new Node(data);
+		} 
+		else 
+		{
+			Node element = new Node(data);
+			Node current = head;
+			while(current.getNext() != null)
+			{
+				current = current.getNext();
+			}
+			current.setNext(element);
+		}
+	}
+	
+	public void display()
+	{
+		Node current = head;
+		System.out.println("Printing the list:");
+		while(current.getNext() != null)
+		{
+			System.out.println(current.getData());
+			current = current.getNext();
+		}
+	}
+	
+	public void addMultiple(Integer... args)
+	{
+		for(Integer arg : args)
+		{
+			this.add(arg);
+		}
+	}
+	
 	public static void main(String[] args) {
 		LinkedList tempLinkedList = new LinkedList(8);
-        System.out.println(tempLinkedList.getHead().getData());
+		tempLinkedList.addMultiple(10, 12, 14, 16);
+        tempLinkedList.display();
+        LinkedList noHeadList = new LinkedList();
+        noHeadList.addMultiple(7, 9, 11, 13, 15);
+        noHeadList.display();
 	}
 
 }
