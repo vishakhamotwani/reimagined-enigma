71b1450f8381a25f0ee8681cd892e229e9830e6e
Vishakha Motwani <vishakha.motwani@gmail.com>
2016-12-10 21:25:28 -0500

    Add method to delete element in a linked list

--- src/psychic/lamp/linkedlist/LinkedList.java
+++ src/psychic/lamp/linkedlist/LinkedList.java
@@ -34,6 +34,24 @@ public class LinkedList {
         head = new Node(item);
     }
 	
+	/**
+	 * Traverses the linked list and displays each element
+	 */
+	public void display()
+	{
+		Node current = head;
+		while(current != null)
+		{
+			System.out.print(current.getData() + " ");
+			current = current.getNext();
+		}
+		System.out.println("\n-----------------\n");
+	}
+	
+	/**
+	 * Adds the given data to the list, makes it a head if the list is empty
+	 * @param data
+	 */
 	public void add(Integer data) 
 	{
 		//If it is the first element in the list
@@ -53,17 +71,10 @@ public class LinkedList {
 		}
 	}
 	
-	public void display()
-	{
-		Node current = head;
-		System.out.println("Printing the list:");
-		while(current.getNext() != null)
-		{
-			System.out.println(current.getData());
-			current = current.getNext();
-		}
-	}
-	
+	/**
+	 * Adds the passed random number of elements to the list
+	 * @param args Elements to be added to the list
+	 */
 	public void addMultiple(Integer... args)
 	{
 		for(Integer arg : args)
@@ -72,12 +83,59 @@ public class LinkedList {
 		}
 	}
 	
+	/**
+	 * The data to be deleted from the list
+	 * @param data
+	 */
+	public void delete(Integer data)
+	{
+		if(head == null)
+		{
+			System.out.println("The list is empty!");
+		} 
+		else if(head.getData() == data)
+		{
+			head = head.getNext();
+		}
+		else
+		{
+			Node current = head.getNext();
+			Node prev = head;
+			while(current != null)
+			{
+				if(current.getData() == data)
+				{
+					prev.setNext(current.getNext());
+					break;
+				}
+				prev = prev.getNext();
+				current = current.getNext();
+			}
+			if(current == null)
+			{
+				System.out.println("Data not found!");
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 		LinkedList tempLinkedList = new LinkedList(8);
-		tempLinkedList.addMultiple(10, 12, 14, 16);
+		tempLinkedList.addMultiple(10, 12, 14, 16, 18, 20);
+        tempLinkedList.display();
+        System.out.println("Deleting 8...");
+        tempLinkedList.delete(8);
         tempLinkedList.display();
         LinkedList noHeadList = new LinkedList();
-        noHeadList.addMultiple(7, 9, 11, 13, 15);
+        noHeadList.addMultiple(7, 9, 11, 13, 15, 17);
+        noHeadList.display();
+        System.out.println("Deleting 13...");
+        noHeadList.delete(13);
+        noHeadList.display();
+        System.out.println("Deleting 17...");
+        noHeadList.delete(17);
+        noHeadList.display();
+        System.out.println("Deleting 19...");
+        noHeadList.delete(19);
         noHeadList.display();
 	}
 
